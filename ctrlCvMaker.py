39cb446e30de9992d4aeb139ce637b47c08c52e1
Mocson <kimura.ryota.as@gmail.com>
2018-11-12 12:11:43 +0900

    項目を追加。

--- ctrlCvMaker.py
+++ ctrlCvMaker.py
@@ -1,10 +1,22 @@
 import maya.cmds as mc
 
-def cvFlatSquare(*arg):
+def cvFlatSquare(*args):
     mc.curve(d=1,p=[(1,0,-1),(-1,0,-1),(-1,0,1),(1,0,1),(1,0,-1)],k=[0,1,2,3,4],n="ctrl1")
 
-def cvFlatCircle(*arg):
+def cvFlatCircle(*args):
     mc.circle(c=(0,0,0),nr=(0,1,0),sw=360,r=1,d=3,ut=True,tol=0.01,s=8,ch=False,n="ctrl1")
+
+def cvFlatTriangle(*args):
+    pass
+
+def cvObjCube(*args):
+    pass
+
+def cvObjSphere(*args):
+    pass
+
+def ctrlLocator(*args):
+    pass
 
 def main():
     if mc.window("ctrlCvMaker", exists=True):
@@ -14,4 +26,8 @@ def main():
     mc.columnLayout(adj=True)
     mc.button(label="Square", command = cvFlatSquare)
     mc.button(label="Circle", command = cvFlatCircle)
+    mc.button(label="Triangle", command = cvFlatTriangle)
+    mc.button(label="Cube", command = cvObjCube)
+    mc.button(label="Sphere", command = cvObjSphere)
+    mc.button(label="Locator", command = ctrlLocator)
     mc.showWindow(win)
